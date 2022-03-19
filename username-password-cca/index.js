6959e9d2011d481effe808e74dfa0f4b7e6a14ac
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2022-03-20 00:13:37 +0530

    add correct tenantID

--- username-password-cca/index.js
+++ username-password-cca/index.js
@@ -4,8 +4,8 @@ var msal = require("@azure/msal-node");
 const msalConf = {
     auth: {
         clientId: "30b7ddff-1254-4834-90ba-4554c4211b2c",
-        // authority: "https://login.microsoftonline.com/07e373ad-d334-46f4-a926-dd2f68a616c0",
-        authority: "https://login.microsoftonline.com/57ebf52b-400a-4db6-a524-b07f4888055a",
+        authority: "https://login.microsoftonline.com/07e373ad-d334-46f4-a926-dd2f68a616c0",
+        // authority: "https://login.microsoftonline.com/57ebf52b-400a-4db6-a524-b07f4888055a",
         clientSecret: "k7G7Q~eE.dSiX~bJ5jeyN~0a5E-IaW5zbpzpp"
     }
 };
