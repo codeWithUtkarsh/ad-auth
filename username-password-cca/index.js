984e309a63177a50e4db0f6245f506845c115550
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2022-03-20 01:09:01 +0530

    code correction

--- username-password-cca/index.js
+++ username-password-cca/index.js
@@ -4,8 +4,8 @@ var msal = require("@azure/msal-node");
 const msalConf = {
     auth: {
         clientId: "30b7ddff-1254-4834-90ba-4554c4211b2c",
-        authority: "https://login.microsoftonline.com/07e373ad-d334-46f4-a926-dd2f68a616c0",
-        // authority: "https://login.microsoftonline.com/57ebf52b-400a-4db6-a524-b07f4888055a",
+        tenant: "07e373ad-d334-46f4-a926-dd2f68a616c0",
+        authority: `https://login.microsoftonline.com/07e373ad-d334-46f4-a926-dd2f68a616c0`,
         clientSecret: "k7G7Q~eE.dSiX~bJ5jeyN~0a5E-IaW5zbpzpp"
     }
 };
@@ -14,8 +14,8 @@ const cca = new msal.ConfidentialClientApplication(msalConf);
 
 const usernamePasswordRequest = {
     scopes: ["user.read"],
-    username: "john@utkarshocthotmailcom.onmicrosoft.com",
-    password: "jd55*poiu",
+    username: "USERNAME",
+    password: "USER_PASS",
 };
 
 cca.acquireTokenByUsernamePassword(usernamePasswordRequest).then((response) => {
